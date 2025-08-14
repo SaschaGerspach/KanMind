96bbd4e5bdae4bb8e87f665cfd7797c362d95837
Sascha Gerspach <sascha.gerspach@gmx.de>
2025-08-14 11:08:31 +0200

    Add GET method for board detail endpoint

--- kanban_app/boards/api/urls.py
+++ kanban_app/boards/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BoardListCreateView
+from .views import BoardListCreateView, BoardDetailView
 
 urlpatterns = [
     path('', BoardListCreateView.as_view(), name='boards-list-create'),
+    path('<int:board_id>/', BoardDetailView.as_view(), name='boards-detail'),
 ]
